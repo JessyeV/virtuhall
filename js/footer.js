b73f98787edb40d4afd1d6e698dbfa395e8e5a52
Jessye <jessye.vergerolle@gmail.com>
2016-01-21 00:09:56 +0100

    Work on footer

---
+++ js/footer.js
@@ -0,0 +1,35 @@
+ $(document).ready(function() {
+
+	 	var $game = $('#game-ctnt').data('toggled','true');
+	 	var $categories = $('#home-ctnt', '#game-ctnt');
+
+	 		if ($categories.is('toggled')){
+	 			$('.right').css({"transform" : "rotate(360deg)"});
+	 	}
+
+		$('#home').on('click',function(){
+			 $('#home-ctnt').slideToggle('300');
+			$('#game-ctnt, #media-ctnt, #com-ctnt, #help-ctnt').slideUp();
+		});
+
+		$('#game').on('click',function(){
+			$('#game-ctnt').slideToggle('300');
+			$('#home-ctnt, #media-ctnt, #com-ctnt, #help-ctnt').slideUp();
+			$('.right').css({"transform" : "rotate(90deg)"});		
+		});
+
+		$('#media').on('click',function(){
+			$('#media-ctnt').slideToggle('300');
+			$('#home-ctnt, #game-ctnt, #com-ctnt, #help-ctnt').slideUp();
+		});
+
+		$('#com').on('click',function(){
+			$('#com-ctnt').slideToggle('300');
+			$('#home-ctnt, #game-ctnt, #media-ctnt, #help-ctnt').slideUp();
+		});
+
+		$('#help').on('click',function(){
+			$('#help-ctnt').slideToggle('300');
+			$('#home-ctnt, #game-ctnt, #media-ctnt, #com-ctnt').slideUp();
+		});
+	});	
