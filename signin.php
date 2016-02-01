0aba0501bb16937c69ccba7220aad580e74087c1
Jessye <jessye.vergerolle@gmail.com>
2016-02-01 23:08:55 +0100

    Signin page - 01/02

--- signin.php
+++ signin.php
@@ -10,5 +10,48 @@
 </head>
 <body>
 	
+	<!-- MENU -->
+	<header class="title-box">
+		<h1>Inscription</h1>
+	</header>
+<div class="row">
+		<div class="main column">
+	<!-- BREADCRUMB -->
+	<nav aria-label="Vous êtes ici:" role="navigation">
+  <ul class="breadcrumbs">
+    <li><a href="#">Accueil</a></li>
+    <li>
+      <span class="show-for-sr">Courant: </span>Inscription
+    </li>
+  </ul>
+</nav>
+
+<!-- MAIN -->
+<h2>Rejoignez-nous en vous inscrivant !</h2>
+<p>Pour jouer à VirtuHall, il vous faut créer un compte qui vous permettra d'accéder au téléchargement du client. <br>
+L’inscription est rapide et facile, alors n’attendez plus ! <br>
+<span class="info">Les champs marqués d’une * sont obligatoires.</span></p>
+
+<!-- FORMULAIRE -->
+<div class="form-head">C'est par ici !</div>
+<form action="" method="POST">
+		<label for="" class="show-for-sr">Pseudo</label>
+		<input type="text" name="pseudo" placeholder="Pseudo*" required></input>
+
+		<label for="" class="show-for-sr">Mot de Passe</label>
+		<input type="password" name="pwd" placeholder="Mot de Passe*" required></input>
+
+		<label for="" class="show-for-sr">E-mail</label>
+		<input type="email" name="mail" placeholder="E-mail*" required></input>
+
+		<label for="" class="show-for-sr">Genre</label>
+		<input type="radio" value="homme" name="gender" class="radio"><span class="radio">&nbsp;&nbsp;Homme</span></input>
+		<input type="radio" value="femme" name="gender">&nbsp;&nbsp;Femme</input><br/>
+
+		<button type="submit" name="" value="valider">Valider l'inscription</button>
+</form>
+</div>
+</div>
+	
 </body>
 </html>
