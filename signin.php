a0bcb08fe13745d8b44dd3db2558d27b851d538a
JessyeV <jessye.vergerolle@gmail.com>
2016-02-01 18:10:45 +0100

    signin first commit

---
+++ signin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+<html lang="fr">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>Inscription</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="dev/foundation-6/css/foundation.min.css">
+	<link rel="stylesheet" href="css/signin.css">
+</head>
+<body>
+	
+</body>
+</html>
