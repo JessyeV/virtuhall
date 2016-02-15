0e85f27cd156edd0509742eb0ce07413dd24eb0d
JessyeV <jessye.vergerolle@gmail.com>
2016-02-15 16:28:07 +0100

    community first commit

---
+++ community.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<head>
+	<html lang="fr">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>Inscription - Virtuhall</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/foundation.min.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/footer.css">
+		<link rel="stylesheet" href="css/community.css">
+
+	</head>
+	<body>
+		<!-- NAVIGATION -->
+		<?php include('header.inc.php'); ?>
+		
+		<!-- MENU -->
+		<header class="title-box">
+			<h1>Communauté</h1>
+		</header>
+		<div class="row" role="main">
+			<div class="main column">
+
+				<!-- BREADCRUMB -->
+				<nav aria-label="Vous êtes ici:" role="navigation">
+					<ul class="breadcrumbs">
+						<li><a href="#">Accueil</a></li>
+						<li>
+							<span class="show-for-sr">Page actuelle: </span>Communauté
+						</li>
+					</ul>
+				</nav>
+
+				<!-- MAIN -->
+				<div class="row" role="main">
+			<div class="main column large-12">
+				<h2>Réseaux sociaux</h2>
+
+
+					<!-- LEFT -->
+					<section class="column large-5">
+						<div>
+							<span class="fb">FB</span>
+							<img src="assets/icons/VH-icon.png" alt="">
+						</div>
+
+						<div>
+							<span class="fb">FB</span>
+							<img src="assets/icons/VH-icon.png" alt="">
+						</div>
+					</section>
+
+					<!-- RIGHT -->
+					<section class="column large-5">
+						<div>
+							<span class="fb">FB</span>
+							<img src="assets/icons/VH-icon.png" alt="">
+						</div>
+						
+						<div>
+							<span class="fb">FB</span>
+							<img src="assets/icons/VH-icon.png" alt="">
+						</div>
+					</section>
+
+					<!-- ADVERTIZING -->
+					<aside class="ad column large-2"></aside>
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+			<!-- FOOTER -->
+			<span class="space"><?php include('footer.inc.php'); ?></span>
+
+			<script src="js/jquery.min.js"></script>
+		</body>
+	</html>
