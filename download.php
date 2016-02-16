2d1e447e9fbc5399cce77057783716478ffae870
JessyeV <jessye.vergerolle@gmail.com>
2016-02-16 16:28:20 +0100

    download first commit

---
+++ download.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+	<html lang="fr">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>Téléchargement - Virtuhall</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/foundation.min.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/footer.css">
+	</head>
+	<body>
+		<!-- NAVIGATION -->
+		<?php include('header.inc.php'); ?>
+		
+		<!-- MENU -->
+		<header class="title-box">
+			<h1>Téléchargement</h1>
+		</header>
+		<div class="row" role="main">
+			<div class="main column">
+
+				<!-- BREADCRUMB -->
+				<nav aria-label="Vous êtes ici:" role="navigation">
+					<ul class="breadcrumbs">
+						<li><a href="#">Accueil</a></li>
+						<li>
+							<span class="show-for-sr">Page actuelle: </span>
+							Téléchargement
+						</li>
+					</ul>
+				</nav>
+
+				<!-- FOOTER -->
+			<span class="space"><?php include('footer.inc.php'); ?></span>
+	</body>
+</html>	
