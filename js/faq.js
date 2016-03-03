6d91f2b3b441c21a72779ad894a4ab00f20469f6
Jessye <jessye.vergerolle@gmail.com>
2016-03-03 22:01:52 +0100

    test (aborted)

--- js/faq.js
+++ js/faq.js
@@ -11,20 +11,13 @@ function openFirstPanel(){
 	$('.accordeon > article > h3').click(function() {
 	$this = $(this);
 	$target =  $this.next();
-	$parent = $this.parent();
-	
-	// $parent.css('background-color', '#2a8ebb');
+
 	if($target.hasClass('active')){
 	$target.removeClass('active').slideUp();
 	}
 	else{
 	allPanels.removeClass('active').slideUp();
 	$target.addClass('active').slideDown();
-	$parent.css('background-color', '#2a8ebb');
-	}
-	
-	if($target.not('active')){
-		$parent.css('background-color', 'lightgrey');
 	}
 
 	return false;
