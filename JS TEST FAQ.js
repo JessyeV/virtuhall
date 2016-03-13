1f856558dcd1e5565ffa7042ab807285a5659fc3
Jessye <jessye.vergerolle@gmail.com>
2016-03-13 20:41:42 +0100

    landing 90%

--- JS TEST FAQ.js
+++
@@ -1,4 +0,0 @@
-	$blue = $parent.css('background-color', '#2a8ebb');
-
-	$parent.add($blue);
-	$parent = $this.parent();
--- landing.php
+++ landing.php
@@ -6,33 +6,41 @@
 		<title>Téléchargement - Virtuhall</title>
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/landing.css">
-		<link rel="stylesheet" href="SSS-master/sss/sss.css" type="text/css" media="all">
+		<link rel="stylesheet" href="css/foundation.min.css">
+		<link rel="stylesheet" href="css/bjqs/bjqs.css">
+		<link rel="stylesheet" href="css/bjqs/slider.css">
 	</head>
 	<body>
 		<div class="main row">
-		<img class="VH-brand" src="assets/icons/VH.png" alt="">
-		<div class="slider">
-			<iframe width="560" height="400" src="https://www.youtube.com/embed/8eBwXuXCOUk?rel=0" frameborder="0" allowfullscreen></iframe>
-			<img src="assets/images/slider/kata.jpg" />
-			<img src="assets/images/slider/tomo.jpg" />
-		</div>
-	
-		<ul class="buttons">
-			<li><a href="">Jouer gratuitement !</a></li>
-			<li><a href="">En découvrir plus...</a></li>
-		</ul>
-
+			<div class="wrapslide">
+			<img class="VH-brand" src="assets/icons/VH.png" alt="">
+			
+			<div id="vh-slider">
+				<ul class="bjqs">
+					<li><div class="embed-container"><iframe width="711" height="400" src="https://www.youtube.com/embed/8eBwXuXCOUk?rel=0" frameborder="0" allowfullscreen></iframe></div></li>
+					<li><img src="assets/images/slider/kata.jpg" /></li>
+					<li><img src="assets/images/slider/tomo.jpg" /></li>
+				</ul>
+			</div>
+			
+			<ul class="buttons">
+				<li class="bluehov"><a href="signin.php">Jouer gratuitement!</a></li>
+				<li class="greenhov"><a href="">Acc<span class="forte">é</span>der au site...</a></li>
+			</ul>
+			</div>
 		<footer class="rad"></footer>
-		</div>
-
-	</body>
-	<!-- SCRIPT -->
-	<script src="js/jquery.min.js"></script>
-	<script src="SSS-master/sss/sss.min.js"></script>
-	<script>
-		jQuery(function($) {
-			$('.slider').sss();
-			slideShow: false;
-		});
-	</script>
+	</div>
+</body>
+<!-- SCRIPT -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bjqs-1.3.js"></script>
+<script>
+	jQuery(document).ready(function($) {
+$('#vh-slider').bjqs({
+	'height' : 400,
+	'width' : 740,
+	'responsive' : true
+});
+});
+</script>
 </html>
