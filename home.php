c17dfc5b88d6352792c07fff61290e4b228eeaa2
JessyeV <jessye.vergerolle@gmail.com>
2016-03-15 09:52:04 +0100

    home page

---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<head>
+	<html lang="fr">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<meta name="description" content="Parce que c'est facile et gratuit, voici tout ce qu'il vous faut pour télécharger VirtuHall le jeu !" />
+		<title>VirtuHall le jeu - Accueil</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/foundation.min.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/footer.css">
+		<link rel="stylesheet" href="css/home.css">
+	</head>
+	<body>
+		<!-- NAVIGATION -->
+		<?php include('header.inc.php'); ?>
+		
+		<!-- MENU -->
+		<header class="title-box">
+			<h1>Accueil</h1>
+		</header>
+		<div class="row" role="main">
+			<div class="main column large-12">
+
+				<!-- MAIN -->
+				<section class="column large-12">
+					<h2>Actualités</h2>
+					<article>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto voluptate cumque ea corrupti nostrum accusamus! Iste nam mollitia dolores quos ea maxime? Reiciendis delectus tenetur voluptatibus, officia tempora. Nobis, temporibus.
+					</article>
+
+					<article>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nulla ea, debitis vel magni, sapiente delectus non excepturi veniam consectetur officia? Nobis commodi rerum deleniti eum voluptatibus vero totam animi.
+					</article>
+
+					<article>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nulla ea, debitis vel magni, sapiente delectus non excepturi veniam consectetur officia? Nobis commodi rerum deleniti eum voluptatibus vero totam animi.
+					</article>
+
+					<article>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nulla ea, debitis vel magni, sapiente delectus non excepturi veniam consectetur officia? Nobis commodi rerum deleniti eum voluptatibus vero totam animi.
+					</article>
+				</section>
+
+				<!-- ADVERTIZING -->
+				<aside class="aside">
+					<ins class="ad-vertical adsbygoogle"
+    					style="display:inline-block;width:160px;height:100%;"
+    					data-ad-client="ca-pub-8371934832180859"
+    					data-ad-slot="1415884923">
+    				</ins>
+
+					<ins class="ad-horizontal adsbygoogle"
+    					style="display:inline-block;width:100%;height:100px;border:5px solid red;"
+   						data-ad-client="ca-pub-8371934832180859"
+    					data-ad-slot="5008479721"></ins>
+				</aside>
+			</div>
+		</div>
+
+		<!-- FOOTER -->
+		<span class="space"><?php include('footer.inc.php'); ?></span>
+
+		<!-- SCRIPT -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/download.js"></script>
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</body>
+</html>
