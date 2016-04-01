e2b786e06041113cf21263fee8d99fe2221d2f38
Jimmy Vergerolle <jimmy.vergerolle@gmail.com>
2016-04-01 05:19:54 +0200

    disable access to download if not logged

--- download.php
+++ download.php
@@ -1,4 +1,10 @@
-<?php session_start(); ?>
+<?php session_start(); 
+
+	if (!isset($_SESSION['pseudo'])) {
+	      header('Location: signin.php');
+	}
+	
+?>
 <!DOCTYPE html>
 <head>
 	<html lang="fr">
