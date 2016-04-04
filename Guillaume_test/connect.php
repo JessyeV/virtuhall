d81c5a15ff3b95684409ed136c875ad4777b9a72
JessyeV <jessye.vergerolle@gmail.com>
2016-04-04 09:17:59 +0200

    removed useless files and files with database logs

--- Guillaume_test/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-		$dbhost = 'virtuhaldv80085.mysql.db';
-		$dbuser = 'virtuhaldv80085';
-		$dbpass = 'Wrongnumber7';
-		$conn = mysqli_connect($dbhost, $dbuser, $dbpass, 'virtuhaldv80085') or die("Erreur de connection");
-	?>
